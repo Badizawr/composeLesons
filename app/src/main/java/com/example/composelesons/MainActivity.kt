1c943da6f17b55a6b4c38a3140bacbf63d383cb2
Badizawr <vicodin-13@ya.ru>
2023-11-23 00:06:25 +0500

    row and column3

--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,10 +22,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(modifier = Modifier.background(Gray)) {
-                Text(text = "Hello!")
-                Text(text = "Moscow")
-                Text(text = "Makeevka")
+            Row(modifier = Modifier
+                .background(Gray)
+                .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Moscow")
+                    Text(text = "Makeevka")
+                }
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Moscow")
+                    Text(text = "Makeevka")
+                }
+
             }
         }
     }
