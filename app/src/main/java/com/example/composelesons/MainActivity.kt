8395269b064b7dda2e99e7441af317e2f4261059
Badizawr <vicodin-13@ya.ru>
2023-11-26 23:45:12 +0500

    finish first lesson

--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelesons.ui.theme.ComposeLesonsTheme
 
@@ -24,19 +28,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             Row(modifier = Modifier
                 .background(Gray)
-                .fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceEvenly) {
-                Column(
+                .fillMaxWidth().fillMaxHeight(0.5f),
+                ) {
+                Column(modifier = Modifier
+                    .background(Red).fillMaxWidth(0.5f).fillMaxHeight(),
                     verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.End
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(text = "Hello!")
                     Text(text = "Moscow")
                     Text(text = "Makeevka")
                 }
-                Column(
+                Column(modifier = Modifier
+                    .background(Green).fillMaxSize(),
                     verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.End
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(text = "Hello!")
                     Text(text = "Moscow")
