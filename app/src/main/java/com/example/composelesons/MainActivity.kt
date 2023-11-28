678f4f69cc7f4cd05b500ee0eda2688d26e22cd7
Badizawr <vicodin-13@ya.ru>
2023-11-28 23:26:45 +0500

    Car,Box,Image

--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -3,53 +3,75 @@ package com.example.composelesons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelesons.ui.theme.ComposeLesonsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(modifier = Modifier
-                .background(Gray)
-                .fillMaxWidth().fillMaxHeight(0.5f),
-                ) {
-                Column(modifier = Modifier
-                    .background(Red).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Moscow")
-                    Text(text = "Makeevka")
-                }
-                Column(modifier = Modifier
-                    .background(Green).fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Moscow")
-                    Text(text = "Makeevka")
+            ListItem("","")
+        }
+    }
+}
+
+
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Box() {
+            Row() {
+                Image(
+                    painter = painterResource(id = R.drawable.ben_stiller),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                    )
+                Column {
+                    Text(text = "Wow")
+                    Text(text = "Wow")
                 }
 
             }
         }
     }
 }
+
