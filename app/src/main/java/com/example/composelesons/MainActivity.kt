0cae6d53c8997e5f94e7d235bc6d2bc4cbe991ba
Badizawr <vicodin-13@ya.ru>
2023-12-06 00:08:48 +0500

    States

--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -29,38 +29,38 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composelesons.ui.theme.ComposeLesonsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { Column(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
         ) {
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
-            ListItem("Ben Stiller","Actor")
+                CircleItem()
         }
         }
     }
@@ -69,40 +69,29 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-private fun ListItem(name: String, prof: String) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .pointerInput(Unit){
-                               detectTapGestures {
-                                   Log.d("MyLog", "Push ME : $it")
-                               }
-
-            },
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(8.dp)
-    ) {
-        Box() {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ben_stiller),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                    )
-                Column(modifier = Modifier.padding(start = 16.dp)){
-                    Text(text = name)
-                    Text(text = prof)
-                }
-
-            }
-        }
+private fun CircleItem() {
+    val die = "Yuo die"
+    var counter = remember{
+        mutableStateOf(0)
     }
+    var color = remember{
+        mutableStateOf(Color.Green)
+    }
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = color.value, shape = CircleShape).clickable {
+                when(++counter.value){
+                    10 -> color.value = Red
+                    20 -> color.value = Blue
+                    30 -> color.value = Cyan
+                    34 -> color.value = Black
+                }
+            },
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = counter.value.toString(), style = TextStyle(color = White, fontSize = 20.sp))
+    }
+
 }
 
