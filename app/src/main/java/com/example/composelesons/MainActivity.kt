8a74d5c322a02a39befd68120dbcebfa7b84104f
Badizawr <vicodin-13@ya.ru>
2023-12-07 23:15:05 +0500

    lazyColumn

--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -1,97 +1,44 @@
 package com.example.composelesons
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.Color.Companion.Yellow
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composelesons.ui.theme.ComposeLesonsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-        ) {
-                CircleItem()
-        }
-        }
-    }
-}
-
-
-
-@Composable
-private fun CircleItem() {
-    val die = "Yuo die"
-    var counter = remember{
-        mutableStateOf(0)
-    }
-    var color = remember{
-        mutableStateOf(Color.Green)
-    }
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .background(color = color.value, shape = CircleShape).clickable {
-                when(++counter.value){
-                    10 -> color.value = Red
-                    20 -> color.value = Blue
-                    30 -> color.value = Cyan
-                    34 -> color.value = Black
+            LazyRow(
+                modifier = Modifier.run {
+                    fillMaxSize()
                 }
-            },
-        contentAlignment = Alignment.Center
-    ){
-        Text(text = counter.value.toString(), style = TextStyle(color = White, fontSize = 20.sp))
+            ){
+               itemsIndexed(
+                   listOf("item1", "item2", "end", "start", "scroll")
+               ){ _, item ->
+                    Text(text = item,
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+               }
+            }
+        }
     }
-
 }
+
+
+
+
 
