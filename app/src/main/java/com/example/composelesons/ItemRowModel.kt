377baa69f7879df2472922cb64d5baafcea030e1
Badizawr <vicodin-13@ya.ru>
2023-12-27 23:28:38 +0500

    text Expand

---
+++ app/src/main/java/com/example/composelesons/ItemRowModel.kt
@@ -0,0 +1,7 @@
+package com.example.composelesons
+
+data class ItemRowModel(
+    val imageId: Int,
+    val title: String,
+    val content: String,
+)
--- app/src/main/java/com/example/composelesons/MainActivity.kt
+++ app/src/main/java/com/example/composelesons/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.composelesons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -13,6 +15,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -20,18 +23,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyRow(
+            LazyColumn(
                 modifier = Modifier.run {
                     fillMaxSize()
+                        .background(Color.Gray)
                 }
             ){
                itemsIndexed(
-                   listOf("item1", "item2", "end", "start", "scroll")
+                   listOf(
+                       ItemRowModel(R.drawable.ben_stiller, "Ben Stiller",
+                           "Best Comedic Actor" + "\nBest Comedic Actor" + "\nBest Comedic Actor" + "\nBest Comedic Actor"),
+                       ItemRowModel(R.drawable.bred_pitt, "Bred Pitt", "test"),
+                       ItemRowModel(R.drawable.jenyfer_enyston, "Enyston",
+                               "No bra club"),
+                       ItemRowModel(R.drawable.dark_wolf, "Dark Wolf",
+                           "The best Python programmer" + "\nThe best Python programmer" + "\nThe best Python programmer" + "\nThe best Python programmer"),
+                       ItemRowModel(R.drawable.johnson_rock, "Rock",
+                           "the pumped-up grandfather"),
+                       ItemRowModel(R.drawable.leonardo_dicaprio, "Dicaprio", "Лёнчик"),
+                       ItemRowModel(R.drawable.scarlett, "Scarlet", "The Black Widow"),
+                       )
                ){ _, item ->
-                    Text(text = item,
-                        fontSize = 30.sp,
-                        modifier = Modifier.padding(horizontal = 10.dp)
-                    )
+                   ItemRow(item = item)
                }
             }
         }
